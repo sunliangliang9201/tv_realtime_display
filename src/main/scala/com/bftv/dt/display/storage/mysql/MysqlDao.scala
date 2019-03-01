e99af2648e5abcfae2db0a23a1b2007fb5996f20
sunliangliang <397585361@qq.com>
2019-03-01 19:42:38 +0800

    'init'

--- src/main/scala/com/bftv/dt/display/storage/mysql/MysqlDao.scala
+++ src/main/scala/com/bftv/dt/display/storage/mysql/MysqlDao.scala
@@ -1,10 +1,11 @@
 package com.bftv.dt.display.storage.mysql
 
 import com.bftv.dt.display.model.SSKeyConf
+import com.bftv.dt.display.utils.ConfigUtil
 import org.slf4j.LoggerFactory
 
 /**
-  * desc
+  * 连接mysql执行各类sql操作
   *
   * @author sunliangliang 2019-03-01 https://github.com/sunliangliang9201/tv_realtime_display
   * @version 1.0
@@ -12,6 +13,13 @@ import org.slf4j.LoggerFactory
 object MysqlDao {
 
   val logger = LoggerFactory.getLogger(this.getClass)
+
+  //测试用，线上用连接池
+  val host = ConfigUtil.getConf.get.getString("mysql_host")
+  val user = ConfigUtil.getConf.get.getString("mysql_user")
+  val passwd = ConfigUtil.getConf.get.getString("mysql_passwd")
+  val db = ConfigUtil.getConf.get.getString("mysql_db")
+  val port = ConfigUtil.getConf.get.getString("mysql_port")
 
   def getSSConf(ssKey: String): SSKeyConf ={
     var ssKeyConf = null
--- src/main/scala/com/bftv/dt/display/utils/ConfigUtil.scala
+++ src/main/scala/com/bftv/dt/display/utils/ConfigUtil.scala
@@ -1,11 +1,19 @@
 package com.bftv.dt.display.utils
 
+import com.typesafe.config.{Config, ConfigFactory}
+
 /**
-  * desc
+  * 返回resources下application.properties中的config对象
   *
   * @author sunliangliang 2019-03-01 https://github.com/sunliangliang9201/tv_realtime_display
   * @version 1.0
   */
 object ConfigUtil {
+
+  val conf = ConfigFactory.load("application.properties")
+
+  def getConf: Option[Config] ={
+    Some(conf)
+  }
 
 }
