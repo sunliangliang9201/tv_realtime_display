202e60a3aff2626ef10e106963bcd385afb9e87f
sunliangliang <397585361@qq.com>
2019-03-01 15:26:55 +0800

    'init'

---
+++ src/main/scala/com/bftv/dt/display/main/TvDisplayMain.scala
@@ -0,0 +1,25 @@
+package com.bftv.dt.display.main
+
+import org.slf4j.LoggerFactory
+
+/**
+  * 主程序入口：包含获取&更新所有配置、获取外部匹配数据如ip解析、创建structured streaming+kafka(SDF)、调用具体的逻辑处理函数s.
+  *
+  * @author sunliangliang 2019-02-22 https://github.com/sunliangliang9201/tv_realtime_display
+  * @version 1.0
+  */
+object TvDisplayMain {
+
+  val logger = LoggerFactory.getLogger(this.getClass)
+
+  def main(args: Array[String]): Unit = {
+    //重传入参数获取key以及是否启用根据offset来获取kafka数据
+    //val sstreamingKey = args(0)
+    //val flag = Integer.valueOf(args(1))
+    val sstreamingKey = "TvDisplay"
+    val flag = 0
+    val sstreamingKeyConf =
+
+
+  }
+}
---
+++ src/main/scala/com/bftv/dt/display/storage/MysqlDao.scala
@@ -0,0 +1,11 @@
+package com.bftv.dt.display.storage
+
+/**
+  * desc
+  *
+  * @author sunliangliang 2018-09-28 https://github.com/sunliangliang9201/tvtest_streaming
+  * @version 1.0
+  */
+object MysqlDao {
+
+}
