3b8755f8921bd91bfe4108b85e14d87ed9a57b09
sunliangliang <397585361@qq.com>
2019-03-01 18:57:07 +0800

    'init'

---
+++ src/main/scala/com/bftv/dt/display/utils/ConfigUtil.scala
@@ -0,0 +1,11 @@
+package com.bftv.dt.display.utils
+
+/**
+  * desc
+  *
+  * @author sunliangliang 2019-03-01 https://github.com/sunliangliang9201/tv_realtime_display
+  * @version 1.0
+  */
+object ConfigUtil {
+
+}
